eef46f7ca63bfe54ee96b2910da76ecf08b50874
Miles Grant <mlsgrnt@gmail.com>
2020-07-08 21:08:43 +0100

    Update splitwise-for-mac.rb

--- Casks/splitwise-for-mac.rb
+++ Casks/splitwise-for-mac.rb
@@ -3,9 +3,9 @@ cask 'splitwise-for-mac' do
   sha256 'fa630132a1c278ca1c37b7f48b659690838461908c4e633711d970780b2b1f26'
 
   name "Splitwise for Mac"
-  url 'https://github.com/mlsgrnt/splitwise-for-mac/releases/download/1.0/Splitwise.app.zip'
-  homepage 'https://github.com/mlsgrnt/splitwise-for-mac'
-  appcast 'https://github.com/mlsgrnt/splitwise-for-mac/releases.atom'
+  url 'https://github.com/mlsgrnt/splitwise-for-mac-and-ios/releases/download/1.0/Splitwise.app.zip'
+  homepage 'https://github.com/mlsgrnt/splitwise-for-mac-and-ios'
+  appcast 'https://github.com/mlsgrnt/splitwise-for-mac-and-ios/releases.atom'
 
   app 'Splitwise.app'
 end
